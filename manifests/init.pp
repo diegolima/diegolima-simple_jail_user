289ca6497f9b13e5eb8d2758746eb49666a30185
rikih <rikih.gunawan@gmail.com>
2014-04-20 23:10:32 +0800

    fix user creation

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,14 @@
 #
 # Init class for the simple_jail_user module.
 #
+# === Authors
+#
+# Rikih Gunawan <rikih dot gunawan at gmail.com>
+#
+# === Copyright
+#
+# Copyright 2014 Rikih Gunawan
+#
 
 class simple_jail_user (
 		$user_data
@@ -13,7 +21,7 @@ class simple_jail_user (
 		ensure => link,
 		target => $bash_bin,
 		owner  => 'root',
-		group  => 'root',
+		group  => "${simple_jail_user::params::user_group}",
 		mode   => '0777',
 	}
 
@@ -53,10 +61,10 @@ class simple_jail_user (
 		file { "${home_dir}":
 			ensure => directory,
 			owner  => 'root',
-			group  => "root",
+			group  => "${simple_jail_user::params::user_group}",
 			mode   => '0755'
 		}	
-		
+
 		file { "${home_dir}/${name}":
 			ensure  => directory,
 			owner   => 'root',
@@ -71,25 +79,27 @@ class simple_jail_user (
 			group   => "${name}",
 			mode    => '0750',
 			content => template("simple_jail_user/profile.erb"),
+			require => File["${home_dir}/${name}"],
 		}
-		
+
+		$user_pass = generate($simple_jail_user::params::bash_bin, '-c', "echo ${password} | ${simple_jail_user::params::openssl} ${simple_jail_user::params::passwd} -1 -stdin | ${simple_jail_user::params::tr} -d '\n'") 
+
 		user { "${name}":
 			ensure           => present,
 			comment          => "Simple jail user $name",
-			managehome       => true,
+			managehome       => false,
 			home             => "${home_dir}/${name}",
-			password         => generate($simple_jail_user::params::bash_bin, '-c', "echo ${password} | ${simple_jail_user::params::openssl} ${simple_jail_user::params::passwd} -1 -stdin | ${simple_jail_user::params::tr} -d '\n'"),
+			password 	 => "${user_pass}",
 			password_min_age => $simple_jail_user::params::password_min_age,
 			shell            => $simple_jail_user::params::rbash_bin,
 			require          => [ 
-									File[$simple_jail_user::params::rbash_bin], 
-								],
-			notify			=>  [
-								  	File["${home_dir}/${name}"], 
-								  	File["${home_dir}/${name}/.profile"],
-								]
+						File[$simple_jail_user::params::rbash_bin], 
+					    ],
+			notify	 	 => [
+						File["${home_dir}/${name}"],
+						File["${home_dir}/${name}/.profile"],
+					    ],
 		}
-
 	}
 
 	define createSymlink () {
@@ -104,36 +114,38 @@ class simple_jail_user (
 		$target = "${tmp[1]}"
 
 		file { "${exec}":
-				ensure => link,
-				owner  => 'root',
-				group  => 'root',
-				mode   => '0777',
-				target => "${target}",
+			ensure => link,
+			owner  => 'root',
+			group  => "${simple_jail_user::params::user_group}",
+			mode   => '0777',
+			target => "${target}",
 		} 
   		
 	}
 	
 	define cleanUpHomeDir ($home_dir) {
   
-	  $unneeded_files = ["$home_dir/$name/.bash_logout", "$home_dir/$name/.bash_profile", "$home_dir/$name/.bashrc"]
-	  $home_dir2 = "$home_dir/$name"
-	  $bin_dir = "$home_dir/$name/bin"
+	  	$unneeded_files = ["$home_dir/$name/.bash_logout", "$home_dir/$name/.bash_profile", "$home_dir/$name/.bashrc"]
+	  	$home_dir2 = "$home_dir/$name"
+	  	$bin_dir = "$home_dir/$name/bin"
 
-	  file { $unneeded_files: ensure => absent, }
+	  	file { $unneeded_files: 
+			ensure => absent, 
+		}
 
-	  file { $bin_dir:
-		ensure => directory,
-		owner  => 'root',
-		group  => 'root',
-		mode   => '0755',
-	  }
+	  	file { $bin_dir:
+			ensure => directory,
+			owner  => 'root',
+			group  => "${simple_jail_user::params::user_group}",
+			mode   => '0755',
+	  	}
 
-	  file { "${home_dir2}/.bash_history":
-		ensure => file,
-		owner  => $name,
-		group  => $name,
-		mode   => '0600',
-	  }
+	  	file { "${home_dir2}/.bash_history":
+			ensure => file,
+			owner  => $name,
+			group  => $name,
+			mode   => '0600',
+		}
 
 	}
 
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,19 +3,28 @@
 #
 # Configuration for the simple_jail_user module. Do not use this class directly.
 #
+# === Authors
+#
+# Rikih Gunawan <rikih dot gunawan at gmail.com>
+#
+# === Copyright
+#
+# Copyright 2014 Rikih Gunawan
+#
 
 class simple_jail_user::params {
 
     case $::operatingsystem {
         /(?i)(Debian|RedHat|CentOS|Ubuntu)/: {
         	
-        	$home_dir			= "/home"
+        	$home_dir		= "/home"
         	$bash_bin   		= "/bin/bash"
-			$rbash_bin  		= "/bin/rbash"
+		$rbash_bin  		= "/bin/rbash"
         	$openssl    		= "openssl"
         	$passwd     		= "passwd"
-        	$tr					= "tr"
+        	$tr			= "tr"
         	$password_min_age 	= "0"
+		$user_group		= "root"
         
         }
         default: {
